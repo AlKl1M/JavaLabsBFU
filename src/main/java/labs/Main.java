489dcff5a176d56045fa9532c1244f7a93ffb8ab
AlKl1M <al.klimkovetskiy@gmail.com>
2023-09-10 11:45:39 +0200

    lab1 done;

--- src/main/java/labs/Main.java
+++ src/main/java/labs/Main.java
@@ -7,6 +7,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println(Lab1.getCollatzSteps(sc.nextInt()));
+        /*
+        int n = sc.nextInt();
+        System.out.println(Lab1.getCollatzSteps(n));
+         */
+
+        /*
+        int n = sc.nextInt();
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = sc.nextInt();
+        }
+        System.out.println(Lab1.calculateAlternatingSignSum(numbers));
+         */
+
+        /*
+        System.out.println(Lab1.findTreasure(sc));
+         */
+
+        /*
+        int[] result = Lab1.findMaxHeight();
+        System.out.println(result[0] + " " + result[1]);
+         */
+
+        int n = sc.nextInt();
+        System.out.println(Lab1.isDoubleEven(n));
     }
 }
